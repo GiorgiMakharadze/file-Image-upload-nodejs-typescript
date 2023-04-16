39e8d2cc5c3c8aa50335c0c0507f39f83b67db0b
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-04-16 15:33:20 +0400

    first commit

--- types/userSchemaTypes.ts
+++
@@ -1,8 +0,0 @@
-export interface IUserSchema extends Document {
-  _id: number;
-  name: string;
-  email: String;
-  password: string;
-  createJWT: () => string;
-  comparePassword: (candidatePassword: string) => Promise<boolean>;
-}
